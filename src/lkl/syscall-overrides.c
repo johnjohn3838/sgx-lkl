d0d57dd831cc3748768c8f423f97d97410eff771
Xuejun Yang <xuejya@microsoft.com>
2020-06-02 02:38:47 +0000

    Overide sysinfo syscall handling

--- src/lkl/syscall-overrides.c
+++ src/lkl/syscall-overrides.c
@@ -1,6 +1,9 @@
+#include <sys/syscall.h>
 #include "lkl/syscall-overrides-futex.h"
 #include "lkl/posix-host.h"
+#include "enclave/enclave_mem.h"
 
 void register_lkl_syscall_overrides()
 {
+    lkl_replace_syscall(SYS_sysinfo, syscall_SYS_sysinfo);
 }
