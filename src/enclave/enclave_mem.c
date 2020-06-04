b49b663b09ad40b7b1c50a7e40fa0cf49f7d73c2
Xuejun Yang <xuejya@microsoft.com>
2020-06-04 19:27:44 +0000

    Address review comments

--- src/enclave/enclave_mem.c
+++ src/enclave/enclave_mem.c
@@ -16,8 +16,6 @@
 #include "enclave/ticketlock.h"
 #include "enclave/sgxlkl_config.h"
 
-static struct sysinfo;
-
 static struct ticketlock mmaplock;
 
 static void* mmap_bitmap;       // Memory allocation bitmap
